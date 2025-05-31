660349541749fa0b776dd6ec88362534d1912899
Lucky <r.matigorov@yandex.ru>
2025-05-31 23:24:24 +0300

    feat: added MainPage.jsx, RequestModal.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import './App.css'
 import Header from "./components/Header.jsx";
+import MainPage from "./pages/MainPage.jsx";
 
 function App() {
-  return (
-      <Header />
-  )
+  return <>
+    <Header />
+    <MainPage/>
+  </>
+
 }
 
 export default App
