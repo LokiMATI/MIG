3b42715efd713bdb36fdef608ae1c6424990dcf9
Lucky <r.matigorov@yandex.ru>
2025-05-31 23:30:16 +0300

    feat: added ServicesPage.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import Header from "./components/Header.jsx";
 import MainPage from "./pages/MainPage.jsx";
+import ServicesPage from "./pages/ServicesPage.jsx";
 
 function App() {
   return <>
     <Header />
     <MainPage/>
+    <ServicesPage/>
   </>
 
 }
